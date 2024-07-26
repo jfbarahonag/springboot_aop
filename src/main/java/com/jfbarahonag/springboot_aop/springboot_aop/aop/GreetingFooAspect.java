8ffd03da570effccf13f40bd6e31a32952acca5b
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-25 22:32:22 -0500

    new aspect added

---
+++ src/main/java/com/jfbarahonag/springboot_aop/springboot_aop/aop/GreetingFooAspect.java
@@ -0,0 +1,10 @@
+package com.jfbarahonag.springboot_aop.springboot_aop.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class GreetingFooAspect {
+
+}
