5fe5e09ad4ec0bf868ded946c039a42065199c77
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-25 22:14:51 -0500

    after returning and after throwing have been added

--- src/main/java/com/jfbarahonag/springboot_aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/jfbarahonag/springboot_aop/springboot_aop/aop/GreetingAspect.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -31,5 +33,25 @@ public class GreetingAspect {
     String args = Arrays.toString(joinPoint.getArgs());
   
     logger.info("Despues: " + method + " con los argumentos " + args);
+  }
+  
+  @AfterReturning("execution(* com.jfbarahonag.springboot_aop.springboot_aop.services.GreetingService.*(..))")
+  public void loggerAfterReturning(JoinPoint joinPoint) {
+    
+    String method = joinPoint.getSignature().getName();
+    String args = Arrays.toString(joinPoint.getArgs());
+  
+    logger.info("Despues Exitoso: " + method + " con los argumentos " + args);
+
+  }
+  
+  @AfterThrowing("execution(* com.jfbarahonag.springboot_aop.springboot_aop.services.GreetingService.*(..))")
+  public void loggerAfterThrowing(JoinPoint joinPoint) {
+    
+    String method = joinPoint.getSignature().getName();
+    String args = Arrays.toString(joinPoint.getArgs());
+  
+    logger.error("Despues fallo: " + method + " con los argumentos " + args);
+
   }
 }
