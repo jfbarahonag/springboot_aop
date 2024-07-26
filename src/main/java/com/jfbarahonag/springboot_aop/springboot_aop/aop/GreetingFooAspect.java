4cd635a98b53dd0e64bc8b0cc60bcc03bd27ee39
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-25 22:39:24 -0500

    order has been applied to the aspects that points to the same service GreetingService

--- src/main/java/com/jfbarahonag/springboot_aop/springboot_aop/aop/GreetingFooAspect.java
+++ src/main/java/com/jfbarahonag/springboot_aop/springboot_aop/aop/GreetingFooAspect.java
@@ -1,10 +1,29 @@
 package com.jfbarahonag.springboot_aop.springboot_aop.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(1)
 @Component
 @Aspect
 public class GreetingFooAspect {
+
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Before("execution(* com.jfbarahonag.springboot_aop.springboot_aop.services.GreetingService.*(..))")
+  public void loggerBefore(JoinPoint joinPoint) {
+
+    String method = joinPoint.getSignature().getName();
+    String args = Arrays.toString(joinPoint.getArgs());
+  
+    logger.info("[Foo] Antes: " + method + " con los argumentos " + args);
+  }
 
 }
